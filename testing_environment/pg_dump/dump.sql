0f3d233edf45dfe9272df8d042da24463694bb10
vladimir <vladimir1012kov@gmail.com>
2024-02-14 18:49:51 +0300

    добавлены новые функции в дамп PostgreSQL

--- testing_environment/pg_dump/dump.sql
+++ testing_environment/pg_dump/dump.sql
@@ -48,6 +48,24 @@ CREATE SCHEMA test_system;
 ALTER SCHEMA test_system OWNER TO kovalenko;
 
 --
+-- Name: get_last_offset(); Type: FUNCTION; Schema: test_system; Owner: kovalenko
+--
+
+CREATE FUNCTION test_system.get_last_offset() RETURNS bigint
+    LANGUAGE sql
+    AS $$
+	select coalesce(
+		(select id_offset
+		from test_system.offset_storage
+		order by id_offset desc
+		limit 1), 0
+	);
+$$;
+
+
+ALTER FUNCTION test_system.get_last_offset() OWNER TO kovalenko;
+
+--
 -- Name: get_messages(bigint, integer); Type: FUNCTION; Schema: test_system; Owner: kovalenko
 --
 
@@ -60,6 +78,7 @@ begin
 		id_offset,
 		message
 	from test_system.messages
+	where id_offset > p_offset
 	order by id_offset
 	limit p_limit;
 end;
@@ -108,6 +127,37 @@ $$;
 
 ALTER PROCEDURE test_system.rec_message(IN p_message jsonb, IN p_offset bigint) OWNER TO kovalenko;
 
+--
+-- Name: update_last_offset(bigint); Type: PROCEDURE; Schema: test_system; Owner: kovalenko
+--
+
+CREATE PROCEDURE test_system.update_last_offset(IN p_offset bigint)
+    LANGUAGE plpgsql
+    AS $$
+	declare 
+		_count int;
+	begin 
+		select count(*) into _count from test_system.offset_storage;
+		if _count = 0 then
+			insert into test_system.offset_storage(
+				id_offset
+			) 
+			values (
+				p_offset
+			);
+		else 
+			update test_system.offset_storage
+			set 
+				id_offset = p_offset,
+				mess_date = CURRENT_TIMESTAMP
+			where id = 1;
+		end if;
+	end;
+$$;
+
+
+ALTER PROCEDURE test_system.update_last_offset(IN p_offset bigint) OWNER TO kovalenko;
+
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
@@ -149,10 +199,52 @@ ALTER SEQUENCE test_system.messages_id_seq OWNED BY test_system.messages.id;
 
 
 --
+-- Name: offset_storage; Type: TABLE; Schema: test_system; Owner: kovalenko
+--
+
+CREATE TABLE test_system.offset_storage (
+    id integer NOT NULL,
+    id_offset bigint,
+    mess_date timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+
+ALTER TABLE test_system.offset_storage OWNER TO kovalenko;
+
+--
+-- Name: offset_storage_id_seq; Type: SEQUENCE; Schema: test_system; Owner: kovalenko
+--
+
+CREATE SEQUENCE test_system.offset_storage_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE test_system.offset_storage_id_seq OWNER TO kovalenko;
+
+--
+-- Name: offset_storage_id_seq; Type: SEQUENCE OWNED BY; Schema: test_system; Owner: kovalenko
+--
+
+ALTER SEQUENCE test_system.offset_storage_id_seq OWNED BY test_system.offset_storage.id;
+
+
+--
 -- Name: messages id; Type: DEFAULT; Schema: test_system; Owner: kovalenko
 --
 
 ALTER TABLE ONLY test_system.messages ALTER COLUMN id SET DEFAULT nextval('test_system.messages_id_seq'::regclass);
+
+
+--
+-- Name: offset_storage id; Type: DEFAULT; Schema: test_system; Owner: kovalenko
+--
+
+ALTER TABLE ONLY test_system.offset_storage ALTER COLUMN id SET DEFAULT nextval('test_system.offset_storage_id_seq'::regclass);
 
 
 --
