7b28d13408241bf12b0bef1e47d387be620d0678
vladimir <vladimir1012kov@gmail.com>
2024-03-12 12:04:07 +0300

    исправлены запросы к БД на восстановление состояния смены и сессии

--- internal/storage/requests.go
+++ internal/storage/requests.go
@@ -3,9 +3,9 @@ package storage
 const (
 	// запросы в БД для сообщений от модуля aggMileageHours
 	// запросы на получение последней смены по objectId
-	getLastObjShift = "SELECT * FROM reports.shifts WHERE object_id = $1 ORDER BY id LIMIT 1"
+	getLastObjShift = "SELECT * FROM reports.shifts WHERE object_id = $1 ORDER BY id DESC LIMIT 1"
 	// запрос на получение последней сессии по object_id
-	getLastObjSession = "SELECT * FROM reports.drivers_sessions WHERE object_id = $1 ORDER BY id LIMIT 1"
+	getLastObjSession = "SELECT * FROM reports.drivers_sessions WHERE object_id = $1 ORDER BY id DESC LIMIT 1"
 	// запрос на добавление новой смены в БД
 	addNewShift = `
 	INSERT INTO reports.shifts (
